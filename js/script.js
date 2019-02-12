e066263b77b56ef6406fbb752d589560804a7aff
Bohdan Kasatkin <bodya226@ukr.net>
2019-02-12 17:13:11 +0200

    added dialog window for login form

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function () {
     $('.physics_slider').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
+        loop: true,
+        margin: 10,
+        nav: true,
         items: 1
-    })
+    });
+    $(function () {
+        $("#login").dialog({
+                autoOpen: false,
+                show: {duration: 200},
+                hide: {duration: 200},
+                resizable: false,
+                draggable: false,
+                position: {at: "center top+70px", of: "#login-btn"}
+            }
+        )
+    });
+    $('#login-btn').click(function () {
+        $("#login").dialog('open');
+    });
+    // $( ".selector" ).dialog( "close" );
 });
